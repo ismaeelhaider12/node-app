2cc6f789d0781dcce378a22a9c1738dc10bb5659
ismaeelhaider12 <ismaeel@eurustechnologies.com>
2021-12-10 17:13:28 +0500

    ismaeelhaider

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Stopping existing node servers
 # echo "Stopping any existing node servers"
-# echo "ismaeel haider you are failed to run this below app"
-# pkill node
+ echo "ismaeel haider you are failed to run this below app"
+ pkill node
