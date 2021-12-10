81440e2723656de07edf2822422f8e0a362c33c9
ismaeelhaider12 <90700806+ismaeelhaider12@users.noreply.github.com>
2021-12-10 15:30:34 +0500

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-#give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/express-app
+#download node and npm
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+# . ~/.nvm/nvm.sh
+# nvm install node
+sudo yum install -y gcc-c++ make 
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+sudo yum install -y nodejs
 
-#navigate into our working directory where we have all our github files
-cd /home/ec2-user/express-app
+#create our working directory if it doesnt exist
+DIR="/home/ec2-user/express-app"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
+fi
 
-#add npm and node to path
-# export NVM_DIR="$HOME/.nvm"	
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
-#install node modules
-npm install
-
-#start our node app in the background
-node app.js > app.out.log 2> app.err.log < /dev/null & 
